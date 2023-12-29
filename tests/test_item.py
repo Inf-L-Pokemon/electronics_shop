07ca6a5783fe631c55e7f36045aeeb952825965c
Sergey Korchmin <sergey.korchmin@yandex.ru>
2023-12-29 21:19:23 +0300

    add tests class Item in test_item.py and some little change after "black"

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,25 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Кофеварка", 25000.00, 2)
+
+
+def test_item_init(item):
+    assert item.name == "Кофеварка"
+    assert item.price == 25000.00
+    assert item.quantity == 2
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 50000.00
+
+
+def test_apply_discount(item):
+    Item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 20000.00
